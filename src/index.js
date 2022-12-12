5fa7d06d1dc24ca706e229690f6228e229aafb5c
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2022-12-12 16:21:13 +0530

    initial comit toolkit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+//what will the output
+const objOne = {
+  type: "poster",
+  mode: "color print"
+};
+
+const objTwo = {
+  type: "poster",
+  mode: "color print"
+};
+
+const objOneCopy = objOne;
+console.log(objOne === objOne);
+console;
