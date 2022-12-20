ce5d2299f52a409c300ded3cf921b9badab03919
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2022-12-20 16:54:03 +0530

    polyfills call,apply,bind

--- src/polyfills.js
+++ src/polyfills.js
@@ -67,3 +67,71 @@ function myForEach(callback) {
 Array.prototype.myForEach = myForEach;
 
 //arr.myForEach(num=>console.log(num*5))
+
+//polyfill for call
+//func.call(obj,arg)
+
+let obj = {
+  name: "sweety",
+  job: "frontend developer"
+};
+
+function displayDetails(age, city) {
+  console.log(`she is ${this.name} ,${this.job} 
+  ,${age},lives in ${city}`);
+}
+
+//const op=displayDetails.call(obj,22,'hyderabad')
+///console.log(op)
+Function.prototype.myCall = myCall;
+
+function myCall(context = {}, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "it's not callable");
+  }
+
+  context.fn = this;
+  context.fn(...args);
+}
+
+//const ans1=displayDetails.myCall(obj,22,'hyderabad')
+//console.log(ans)
+
+//polyfill for apply
+
+Function.prototype.myApply = myApply;
+
+function myApply(context = {}, args = []) {
+  if (typeof this !== "function") {
+    throw new Error(this + "it's not callable");
+  }
+
+  if (!Array.isArray(args)) {
+    throw new TypeError("createListFromArrayLike called on non-object");
+  }
+
+  context.fn = this;
+  context.fn(...args);
+}
+
+//const ans2=displayDetails.myApply(obj,[22,'hyderabad'])
+//console.log(ans2)
+
+//polyfill for bind
+
+Function.prototype.myBind = myBind;
+
+function myBind(context = {}, ...args) {
+  if (typeof this !== "function") {
+    throw new Error(this + "it's not callable");
+  }
+
+  context.fn = this;
+  return function (...args) {
+    return context.fn(...args);
+  };
+}
+
+const ans3 = displayDetails.bind(obj, 22, "pune");
+
+ans3();
