cfc0016dd24390381a3bab23f9340c82b285a6a0
swetha_555 <65771591+swethaogeti@users.noreply.github.com>
2022-12-25 23:22:25 +0530

    practice

--- src/js-objects/prototype.js
+++ src/js-objects/prototype.js
@@ -24,3 +24,67 @@ p2.sayHi = function () {
 
 let sayHi = p2.sayHi();
 console.log(sayHi);
+
+//for in 
+
+// for(const prop in p1){
+//   console.log(prop ,p1[prop])
+// } 
+
+var decoration={
+  color:'red'
+}
+
+var circle=Object.create(decoration)
+
+circle.radius=10;
+
+// for(const prop in circle){
+//   console.log(prop)
+// }
+
+for(const prop in circle){
+  if(circle.hasOwnProperty(prop)){
+    console.log(prop)
+  }
+}
+
+
+//enmerable properties
+
+const p3={
+  firstName:'jhon',
+  lastName:'doe'
+}
+
+p3.age=22
+Object.defineProperty(p3,'ssn',
+{enumerable:false,value:1212122121})
+
+for(const key in p3){
+  console.log(key)
+}
+
+console.log(p3.propertyIsEnumerable('firstName'))
+
+//Object.values()
+
+const p4={
+  name:'sweety',
+  age:22
+}
+
+const profile=Object.values(p4)
+console.log(profile)
+
+//Object.entries()
+const ssn=Symbol('ssn')
+const p5={
+  name:'sweety',
+  lastName:'kate',
+  age:22,
+[ssn]:'23816371253'
+
+}
+const kv=Object.entries(p5)
+console.log(kv)
